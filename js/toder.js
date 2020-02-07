4c9b605fd1bc14230b653c77a801b94c8c3dd117
Łukasz Kochański <kochanski90@gmail.com>
2020-02-07 18:38:05 +0100

    added toder.js class constructor

--- js/toder.js
+++ js/toder.js
@@ -1,4 +1,31 @@
 class Toder {
   constructor() {
+    this.init();
+  }
+  init(){
+    this.taskNameInput = document.querySelector('#taskName');
+    this.taskDescInput = document.querySelector('#taskDesc');
+    this.toDoPanel = document.querySelector('#toDoPanel');
+    this.inProgressPanel = document.querySelector('#inProgressPanel');
+    this.donePanel = document.querySelector('#donePanel');
+    this.addBtn = document.querySelector('#addTaskBtn');
+  }
+
+  addTask(){
+
+  }
+
+  removeTask(){
+
+  }
+
+  moveToInProgress(){
+
+  }
+
+  moveToDone(){
+
   }
 }
+
+const toDoApp = new Toder();
